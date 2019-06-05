ebba613bd92b63dc2c3d64a0b1014d3916343a49
Brian Berns <brianberns@gmail.com>
2019-06-04 21:12:27 -0400

    Implemented multiplication.

--- Bernsrite.PowerSeries/PowerSeries.fs
+++ Bernsrite.PowerSeries/PowerSeries.fs
@@ -1,9 +1,28 @@
 namespace Bernsrite.PowerSeries
 
-type PowerSeries<'T when ^T : (static member (~-) : ^T -> ^T)> =
+module List =
+
+    let (|Cons|Nil|) = function
+        | [] -> Nil
+        | head :: tail -> Cons(head, tail)
+
+type PowerSeries<'T> =
     | (::) of ('T * Lazy<PowerSeries<'T>>)
 
 module PowerSeries =
+
+    let inline toString series =
+        let rec loop level series =
+            match series with
+                | f :: _ when level = 0 -> sprintf "%A, ..." f
+                | f :: fs -> sprintf "%A, %s" f (fs.Value |> loop (level - 1))
+        series |> loop 3
+
+    let rec take n (f :: fs) =
+        if n <= 0 then
+            []
+        else
+            List.Cons(f, fs.Value |> take (n-1))
 
     let rec zero =
         0 :: lazy zero
@@ -11,26 +30,33 @@ module PowerSeries =
     let ofInt n =
         n :: lazy zero
 
+    let rec ofList ns =
+        match ns with
+            | List.Nil -> zero
+            | List.Cons (head, tail) -> head :: lazy (ofList tail)
+
     let rec x =
         0 :: lazy (1 :: lazy zero)
 
-    let rec negate = function
-        | f :: fs -> -f :: lazy (negate fs.Value)
+    let rec scale c (f :: fs) =
+        c * f :: lazy (scale c fs.Value)
 
-    let inline toString<'T when ^T : (static member (~-) : ^T -> ^T)> (series : PowerSeries<'T>) =
-        let rec loop level series =
-            match series with
-                | f :: _ when level = 0 -> sprintf "%A, ..." f
-                | f :: fs -> sprintf "%A, %s" f (fs.Value |> loop (level - 1))
-        series |> loop 3
+    let rec add (f :: fs) (g :: gs) =
+        (f + g) :: lazy (add fs.Value gs.Value)
 
-type PowerSeries<'T when ^T : (static member (~-) : ^T -> ^T)> with
+    let rec mult (f :: fs) (g :: gs) =
+        f * g :: lazy (add (scale f gs.Value) (mult fs.Value (g :: gs)))
+
+type PowerSeries<'T> with
 
     static member inline Zero =
         PowerSeries.zero
 
-    static member inline (~-) series =
-        PowerSeries.negate series
+    static member inline (.*)(c, series) =
+        PowerSeries.scale c series
 
     static member inline (+)(seriesA, seriesB) =
-        ()
+        PowerSeries.add seriesA seriesB
+
+    static member inline (*)(seriesA, seriesB) =
+        PowerSeries.mult seriesA seriesB
--- UnitTests/UnitTests.fs
+++ UnitTests/UnitTests.fs
@@ -7,4 +7,8 @@ type UnitTest() =
 
     [<TestMethod>]
     member __.Test1() =
-        printfn "%s" (PowerSeries.zero |> PowerSeries.toString)
+        let series = [1; 0; -2] |> PowerSeries.ofList
+        let series = series * series * series
+        Assert.AreEqual(
+            [1; 0; -6; 0; 12; 0; -8; 0; 0; 0],
+            series |> PowerSeries.take 10)
