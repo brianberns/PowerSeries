5c756f224698a576d7653b14390c3ccd6a6e9d29
Brian Berns <brianberns@gmail.com>
2019-06-05 16:45:22 -0400

    Improved tests.

--- UnitTests/UnitTests.fs
+++ UnitTests/UnitTests.fs
@@ -10,68 +10,50 @@ open MathNet.Numerics
 [<TestClass>]
 type UnitTest() =
 
-    [<TestMethod>]
-    member __.Types() =
+    member inline __.TypeTest<'T
+                        when ^T : (static member Zero : ^T)
+                        and ^T : (static member One : ^T)
+                        and ^T : (static member (+) : ^T * ^T -> ^T)
+                        and ^T : (static member (*) : ^T * ^T -> ^T)
+                        and ^T : (static member (/) : ^T * ^T -> ^T)
+                        and ^T : (static member (~-) : ^T -> ^T)
+                        and ^T : equality>(cast : int -> 'T) =
 
         let coeff = [1; 0; -2]
         let expected = [1; 0; -6; 0; 12; 0; -8; 0; 0; 0]
 
-        let cast = id
         let series =
-            coeff |> List.map cast |> PowerSeries.ofList
-        let series = series ** 3
+            coeff
+                |> List.map cast
+                |> PowerSeries.ofList
         Assert.AreEqual(
             expected |> List.map cast,
-            series |> PowerSeries.take 10)
+            series ** 3 |> PowerSeries.take expected.Length)
 
-        let cast = int64
-        let series =
-            coeff |> List.map cast |> PowerSeries.ofList
-        let series = series ** 3
-        Assert.AreEqual(
-            expected |> List.map cast,
-            series |> PowerSeries.take 10)
-
-        let cast (x : int) = BigInteger(x)
-        let series =
-            coeff |> List.map cast |> PowerSeries.ofList
-        let series = series ** 3
-        Assert.AreEqual(
-            expected |> List.map cast,
-            series |> PowerSeries.take 10)
-
-        let cast = decimal
-        let series =
-            coeff |> List.map cast |> PowerSeries.ofList
-        let series = series ** 3
-        Assert.AreEqual(
-            expected |> List.map cast,
-            series |> PowerSeries.take 10)
-
-        let cast = BigRational.FromInt
-        let series =
-            coeff |> List.map cast |> PowerSeries.ofList
-        let series = series ** 3
-        Assert.AreEqual(
-            expected |> List.map cast,
-            series |> PowerSeries.take 10)
+    [<TestMethod>]
+    member this.Types() =
+        this.TypeTest(id)
+        this.TypeTest(int64)
+        this.TypeTest(BigInteger)
+        this.TypeTest(decimal)
+        this.TypeTest(BigRational.FromInt)
 
     [<TestMethod>]
     member __.Expression() =
 
         let x = PowerSeries<int>.X
-        let series = (1 - (2 * (x ** 2))) ** 3
+        let series = (1 - 2*x**2) ** 3
         printfn "%A" <| PowerSeries.toString series
         Assert.AreEqual(
             [1; 0; -6; 0; 12; 0; -8; 0; 0; 0],
             series |> PowerSeries.take 10)
 
-        let series = 1 / (1 - x)
+        let series = 1 / (1-x)
         Assert.AreEqual(
             [1; 1; 1; 1; 1; 1; 1; 1; 1; 1],
             series |> PowerSeries.take 10)
 
-        let series = 1 / (1 - x) ** 2
+        let series = 1 / (1-x)**2
         Assert.AreEqual(
             [1; 2; 3; 4; 5; 6; 7; 8; 9; 10],
             series |> PowerSeries.take 10)
@@ -92,9 +74,10 @@ type UnitTest() =
 
     [<TestMethod>]
     member __.Exp() =
+        let expected = [1N; 1N; 1N/2N; 1N/6N; 1N/24N; 1N/120N; 1N/720N]
         Assert.AreEqual(
-            [1N; 1N; 1N/2N; 1N/6N; 1N/24N; 1N/120N; 1N/720N],
-            PowerSeries.exp |> PowerSeries.take 7)
+            expected,
+            PowerSeries.exp |> PowerSeries.take expected.Length)
         Assert.AreEqual(
             Math.E,
             PowerSeries.exp
