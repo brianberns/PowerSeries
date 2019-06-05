138fc803b543d00970afbde8d1e26f19b0473ba9
Brian Berns <brianberns@gmail.com>
2019-06-04 22:46:25 -0400

    Made generic. Painful.

--- Bernsrite.PowerSeries/PowerSeries.fs
+++ Bernsrite.PowerSeries/PowerSeries.fs
@@ -1,56 +1,85 @@
 namespace Bernsrite.PowerSeries
 
+open System
+open Microsoft.FSharp.Core.LanguagePrimitives
+
 module List =
 
     let (|Cons|Nil|) = function
         | [] -> Nil
         | head :: tail -> Cons(head, tail)
 
-type PowerSeries<'T> =
+type PowerSeries<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> =
     | (::) of ('T * Lazy<PowerSeries<'T>>)
 
 module PowerSeries =
 
     let inline toString series =
-        let rec loop level series =
-            match series with
-                | f :: _ when level = 0 -> sprintf "%A, ..." f
-                | f :: fs -> sprintf "%A, %s" f (fs.Value |> loop (level - 1))
+        let rec loop level = function
+            | f :: _ when level = 0 ->
+                sprintf "%A, ..." f
+            | f :: fs ->
+                sprintf "%A, %s" f (fs.Value |> loop (level - 1))
         series |> loop 3
 
-    let rec take n (f :: fs) =
-        if n <= 0 then
-            []
-        else
-            List.Cons(f, fs.Value |> take (n-1))
+    let inline take<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> n series =
+        let rec loop n (f : 'T :: fs) =
+            if n <= 0 then
+                []
+            else
+                List.Cons(f, fs.Value |> loop (n-1))
+        loop n series
 
-    let rec zero =
-        0 :: lazy zero
+    let inline zero<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> =
+        let rec value =  GenericZero<'T> :: lazy value
+        value
+
+    let inline one<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> =
+        let rec value = GenericOne<'T> :: lazy zero
+        value
 
     let ofInt n =
-        n :: lazy zero
+        n :: lazy zero<int>
 
-    let rec ofList ns =
-        match ns with
+    let inline ofList<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> (ns : List<'T>) =
+        let rec loop = function
             | List.Nil -> zero
-            | List.Cons (head, tail) -> head :: lazy (ofList tail)
+            | List.Cons (head, tail) -> head :: lazy (loop tail)
+        ns |> loop
 
-    let rec x =
-        0 :: lazy (1 :: lazy zero)
+    let inline x<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> =
+        let rec value = GenericZero<'T> :: lazy (GenericOne<'T> :: lazy zero)
+        value
 
-    let rec scale c (f :: fs) =
-        c * f :: lazy (scale c fs.Value)
+    let inline scale (c : 'T) series =
+        let rec loop (f :: fs) =
+            c * f :: lazy (fs.Value |> loop)
+        series |> loop
 
-    let rec add (f :: fs) (g :: gs) =
-        (f + g) :: lazy (add fs.Value gs.Value)
+    let inline add seriesA seriesB =
+        let rec loop (f : 'T :: fs) (g : 'T :: gs) =
+            (f + g) :: lazy (loop fs.Value gs.Value)
+        loop seriesA seriesB
 
-    let rec mult (f :: fs) (g :: gs) =
-        f * g :: lazy (add (scale f gs.Value) (mult fs.Value (g :: gs)))
+    let inline mult seriesA seriesB =
+        let rec loop (f : 'T :: fs) (g : 'T :: gs) =
+            f * g :: lazy (add (scale f gs.Value) (loop fs.Value (g :: gs)))
+        loop seriesA seriesB
 
-type PowerSeries<'T> with
+    let inline pow n series =
+        let rec loop n series =
+            match n with
+                | 0 -> one
+                | n when n > 0 ->
+                    let right = series |> loop (n - 1)
+                    mult series right
+                | _ -> raise <| NotSupportedException()
+        series |> loop n
+
+type PowerSeries<'T when ^T : (static member Zero : ^T) and ^T : (static member One : ^T) and ^T : (static member (+) : ^T * ^T -> ^T) and ^T : (static member (*) : ^T * ^T -> ^T)> with
 
     static member inline Zero =
-        PowerSeries.zero
+        PowerSeries.zero<'T>
 
     static member inline (.*)(c, series) =
         PowerSeries.scale c series
@@ -60,3 +89,6 @@ type PowerSeries<'T> with
 
     static member inline (*)(seriesA, seriesB) =
         PowerSeries.mult seriesA seriesB
+
+    static member inline Pow(series, n) =
+        PowerSeries.pow n series
--- UnitTests/UnitTests.fs
+++ UnitTests/UnitTests.fs
@@ -1,5 +1,6 @@
 ﻿namespace Bernsrite.PowerSeries
 
+open System
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
@@ -8,7 +9,7 @@ type UnitTest() =
     [<TestMethod>]
     member __.Test1() =
         let series = [1; 0; -2] |> PowerSeries.ofList
-        let series = series * series * series
+        let series = series ** 3
         Assert.AreEqual(
             [1; 0; -6; 0; 12; 0; -8; 0; 0; 0],
             series |> PowerSeries.take 10)
