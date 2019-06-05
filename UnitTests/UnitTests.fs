54bb240cca3bae8d5e4c83c538d5947b42b87b12
Brian Berns <brianberns@gmail.com>
2019-06-04 22:58:23 -0400

    Test power series of different underlying types.

--- UnitTests/UnitTests.fs
+++ UnitTests/UnitTests.fs
@@ -1,6 +1,6 @@
 ﻿namespace Bernsrite.PowerSeries
 
-open System
+open System.Numerics
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 [<TestClass>]
@@ -8,8 +8,30 @@ type UnitTest() =
 
     [<TestMethod>]
     member __.Test1() =
-        let series = [1; 0; -2] |> PowerSeries.ofList
+
+        let coeff = [1; 0; -2]
+        let expected = [1; 0; -6; 0; 12; 0; -8; 0; 0; 0]
+
+        let cast = id
+        let series =
+            coeff |> List.map cast |> PowerSeries.ofList
         let series = series ** 3
         Assert.AreEqual(
-            [1; 0; -6; 0; 12; 0; -8; 0; 0; 0],
+            expected |> List.map cast,
+            series |> PowerSeries.take 10)
+
+        let cast = int64
+        let series =
+            coeff |> List.map cast |> PowerSeries.ofList
+        let series = series ** 3
+        Assert.AreEqual(
+            expected |> List.map cast,
+            series |> PowerSeries.take 10)
+
+        let cast (x : int) = BigInteger(x)
+        let series =
+            coeff |> List.map cast |> PowerSeries.ofList
+        let series = series ** 3
+        Assert.AreEqual(
+            expected |> List.map cast,
             series |> PowerSeries.take 10)
