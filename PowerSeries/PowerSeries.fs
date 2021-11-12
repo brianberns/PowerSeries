ef90960477934e6ff2420792abef57625567fadc
Brian Berns <brianberns@gmail.com>
2021-11-12 18:21:51 -0500

    Simplified by moving core behavior into member functions.

--- PowerSeries/PowerSeries.fs
+++ PowerSeries/PowerSeries.fs
@@ -14,6 +14,9 @@ module List =
         | [] -> Nil
         | head :: tail -> Cons(head, tail)
 
+#nowarn "40"   // recursive values
+#nowarn "77"   // F# compiler bug workaround
+
 /// A power series: a0 + a1*x + a2*x^2 + a3*x^3 + ...
 type PowerSeries<'T
         when ^T : (static member Zero : ^T)
@@ -25,198 +28,100 @@ type PowerSeries<'T
         and ^T : equality> =
     | (::) of 'T * Lazy<PowerSeries<'T>>
 
-module internal Internal =
-
     /// Power series for 0.
-    let inline zero<'T
-            when ^T : (static member Zero : ^T)
-            and ^T : (static member One : ^T)
-            and ^T : (static member (+) : ^T * ^T -> ^T)
-            and ^T : (static member (*) : ^T * ^T -> ^T)
-            and ^T : (static member (/) : ^T * ^T -> ^T)
-            and ^T : (static member (~-) : ^T -> ^T)
-            and ^T : equality> =
+    static member inline Zero =
         let rec value = GenericZero<'T> :: lazy value
         value
 
     /// Power series for a constant.
-    let inline constant<'T
-                when ^T : (static member Zero : ^T)
-                and ^T : (static member One : ^T)
-                and ^T : (static member (+) : ^T * ^T -> ^T)
-                and ^T : (static member (*) : ^T * ^T -> ^T)
-                and ^T : (static member (/) : ^T * ^T -> ^T)
-                and ^T : (static member (~-) : ^T -> ^T)
-                and ^T : equality> (c : 'T) =
-        c :: lazy zero
+    static member inline Constant(c : 'T) =
+        c :: lazy PowerSeries<'T>.Zero
 
     /// Power series for 1.
-    let inline one<'T
-            when ^T : (static member Zero : ^T)
-            and ^T : (static member One : ^T)
-            and ^T : (static member (+) : ^T * ^T -> ^T)
-            and ^T : (static member (*) : ^T * ^T -> ^T)
-            and ^T : (static member (/) : ^T * ^T -> ^T)
-            and ^T : (static member (~-) : ^T -> ^T)
-            and ^T : equality> =
-        constant GenericOne<'T>
+    static member inline One =
+        PowerSeries.Constant(GenericOne<'T>)
 
-    /// Power serious for a variable x = 0 + 1*x.
-    let inline x<'T
-            when ^T : (static member Zero : ^T)
-            and ^T : (static member One : ^T)
-            and ^T : (static member (+) : ^T * ^T -> ^T)
-            and ^T : (static member (*) : ^T * ^T -> ^T)
-            and ^T : (static member (/) : ^T * ^T -> ^T)
-            and ^T : (static member (~-) : ^T -> ^T)
-            and ^T : equality> =
-        GenericZero<'T> :: lazy one
+    /// Power series for a variable x = 0 + 1*x.
+    static member inline X =
+        GenericZero<'T> :: lazy PowerSeries.One
 
     /// Constructs a power series from the given coeffecients.
-    let inline ofList<'T
-            when ^T : (static member Zero : ^T)
-            and ^T : (static member One : ^T)
-            and ^T : (static member (+) : ^T * ^T -> ^T)
-            and ^T : (static member (*) : ^T * ^T -> ^T)
-            and ^T : (static member (/) : ^T * ^T -> ^T)
-            and ^T : (static member (~-) : ^T -> ^T)
-            and ^T : equality> (ns : List<'T>) =
+    static member inline OfList(ns : List<'T>) =
         let rec loop = function
-            | List.Nil -> zero
+            | List.Nil -> PowerSeries.Zero
             | List.Cons (head, tail) -> head :: lazy (loop tail)
         ns |> loop
 
     /// Negates the given power series.
-    let inline negate series =
+    static member inline (~-)(series) =
         let rec loop (f :: fs) =
             -f :: lazy (loop fs.Value)
         loop series
 
-    /// Scales the given power series by a constant.
-    let inline scale (c : 'T) series =
-        let rec loop (f :: fs) =
-            (c * f) :: lazy (loop fs.Value)
-        loop series
-
     /// Adds the given power series.
-    let inline add seriesF seriesG =
+    static member inline (+)(seriesF, seriesG) =
+        let (.+) a b =
+            (^T : (static member (+) : ^T * ^T -> ^T)(a, b))   // F# compiler bug workaround
         let rec loop (f : 'T :: fs) (g : 'T :: gs) =
-            (f + g) :: lazy (loop fs.Value gs.Value)
+            (f .+ g) :: lazy (loop fs.Value gs.Value)
         loop seriesF seriesG
 
+    /// Adds the given constant value to the given power series.
+    static member inline (+)(value : 'T, series) =
+        PowerSeries.Constant(value) + series
+
     /// Subtracts the given power series.
-    let inline sub seriesF seriesG =
-        add seriesF (negate seriesG)
+    static member inline (-)(seriesF, seriesG) =
+        seriesF + (-seriesG)
+
+    /// Subtracts the given power series from the given constant value.
+    static member inline (-)(value : 'T, series) =
+        PowerSeries.Constant(value) - series
+
+    /// Multiplies the given power series by a constant.
+    static member inline (*)(c : 'T, series) =
+        let (.*) a b =
+            (^T : (static member (*) : ^T * ^T -> ^T)(a, b))   // F# compiler bug workaround
+        let rec loop (f :: fs) =
+            (c .* f) :: lazy (loop fs.Value)
+        loop series
 
     /// Multiplies the given power series.
-    let inline mult seriesF seriesG =
+    static member inline (*)(seriesF, seriesG) =
+        let (.*) a b =
+            (^T : (static member (*) : ^T * ^T -> ^T)(a, b))   // F# compiler bug workaround
         let rec loop (f : 'T :: fs) (g : 'T :: gs) =
-            (f * g) :: lazy (add (scale f gs.Value) (loop fs.Value (g :: gs)))
+            (f .* g) :: lazy ((f * gs.Value) + (loop fs.Value (g :: gs)))
         loop seriesF seriesG
 
     /// Divides the given power series.
-    let inline div seriesF seriesG =
+    static member inline (/)(seriesF, seriesG) =
         let rec loop (f : 'T :: fs) (g : 'T :: gs) =
             if f = GenericZero<'T> && g = GenericZero<'T> then
                 loop fs.Value gs.Value
             else
                 let q = f / g
-                q :: lazy (loop (sub fs.Value (scale q gs.Value)) (g :: gs))
+                q :: lazy (loop (fs.Value - (q * gs.Value)) (g :: gs))
         loop seriesF seriesG
 
-    /// Raises the given power series to a power.
-    let inline pow n series =
-        let rec loop n series =
-            match n with
-                | 0 -> one
-                | n when n > 0 ->
-                    series
-                        |> loop (n - 1)
-                        |> mult series
-                | _ -> failwith "Negative exponents not supported"
-        series |> loop n
-
-/// Power series extensions.
-type PowerSeries<'T
-        when ^T : (static member Zero : ^T)
-        and ^T : (static member One : ^T)
-        and ^T : (static member (+) : ^T * ^T -> ^T)
-        and ^T : (static member (*) : ^T * ^T -> ^T)
-        and ^T : (static member (/) : ^T * ^T -> ^T)
-        and ^T : (static member (~-) : ^T -> ^T)
-        and ^T : equality> with
-
-    /// Power series for 0.
-    static member inline Zero =
-        Internal.zero<'T>
-
-    /// Power series for 1.
-    static member inline One =
-        Internal.one<'T>
-
-    /// Creates a power series for the given value.
-    static member inline OfValue(value : 'T) =
-        Internal.constant(value)
-
-    /// 0 + 1*x
-    static member inline X =
-        Internal.x<'T>
-
-    /// Negates the given power series.
-    static member inline (~-) series =
-        Internal.negate series
-
-    /// Adds the given power series.
-    static member inline (+)(seriesF : PowerSeries<'T>, seriesG : PowerSeries<'T>) =
-        Internal.add seriesF seriesG
-
-    /// Adds the given constant value to the given power series.
-    static member inline (+)(value : 'T, series : PowerSeries<'T>) =
-        (Internal.constant value) + series
-
-    /// Subtracts one given power series from the other.
-    static member inline (-)(seriesF : PowerSeries<'T>, seriesG : PowerSeries<'T>) =
-        Internal.sub seriesF seriesG
-
-    /// Subtracts the given power series from the given constant value.
-    static member inline (-)(value : 'T, series : PowerSeries<'T>) =
-        (Internal.constant value) - series
-
-    /// Multiplies the given power series.
-    static member inline (*)(seriesF : PowerSeries<'T>, seriesG : PowerSeries<'T>) =
-        Internal.mult seriesF seriesG
-
-    /// Multiplies the given power series by the given constant.
-    static member inline (*)(value : 'T, series : PowerSeries<'T>) =
-        (Internal.constant value) * series
-
-    /// Divides the given power series.
-    static member inline (/)(seriesF : PowerSeries<'T>, seriesG : PowerSeries<'T>) =
-        Internal.div seriesF seriesG
-
-    /// Divides the given constant by the given power series.
-    static member inline (/)(value : 'T, series : PowerSeries<'T>) =
-        (Internal.constant value) / series
+    /// Divides the given constant value by the given power series.
+    static member inline (/)(value : 'T, series) =
+        PowerSeries.Constant(value) / series
 
     /// Raises the given power series to a power.
     static member inline Pow(series, n) =
-        Internal.pow n series
-
-module PowerSeries =
-
-    /// Constructs a power series from the given coeffecients.
-    let inline ofList ns =
-        Internal.ofList ns
+        let rec loop n series =
+            match n with
+                | 0 -> PowerSeries.One
+                | n when n > 0 ->
+                    series
+                        |> loop (n - 1)
+                        |> (*) series
+                | _ -> failwith "Negative exponents not supported"
+        series |> loop n
 
     /// Takes a finite number of coeffecients from the given power series.
-    let inline take<'T
-            when ^T : (static member Zero : ^T)
-            and ^T : (static member One : ^T)
-            and ^T : (static member (+) : ^T * ^T -> ^T)
-            and ^T : (static member (*) : ^T * ^T -> ^T)
-            and ^T : (static member (/) : ^T * ^T -> ^T)
-            and ^T : (static member (~-) : ^T -> ^T)
-            and ^T : equality> n series =
+    member inline series.Take(n) =
         let rec loop n (f : 'T :: fs) =
             if n <= 0 then
                 []
@@ -225,10 +130,11 @@ module PowerSeries =
         loop n series
 
     /// Display string.
-    let inline toString series =
-        series
-            |> take 3
+    member inline series.String =
+        series.Take(3)
             |> sprintf "%A, ..."
+
+module PowerSeries =
 
     /// Composes two power series: F(G).
     let inline compose seriesF seriesG =
@@ -258,7 +164,7 @@ module PowerSeries =
 
     /// Answers the integral of the given power series.
     let inline private lazyIntegral (fs : Lazy<_>) =
-        let rec int1 (g : 'T :: gs) n : PowerSeries<'T> =
+        let rec int1 (g : 'T :: gs) n =
             (g / n) :: lazy (int1 gs.Value (n + GenericOne<'T>))
         GenericZero<'T> :: lazy (int1 fs.Value GenericOne<'T>)
 
@@ -273,7 +179,7 @@ module PowerSeries =
             if n <= 0 then
                 GenericZero<'T>
             else
-                f + (fs.Value |> Internal.scale x |> loop (n - 1))
+                f + (x * fs.Value |> loop (n - 1))
         loop n series
 
     /// Exponential function.
--- UnitTests/UnitTests.fs
+++ UnitTests/UnitTests.fs
@@ -25,10 +25,10 @@ type UnitTest() =
         let series =
             coeff
                 |> List.map cast
-                |> PowerSeries.ofList
+                |> PowerSeries.OfList
         Assert.AreEqual(
             expected |> List.map cast,
-            series ** 3 |> PowerSeries.take expected.Length)
+            (series ** 3).Take(expected.Length))
 
     [<TestMethod>]
     member this.Types() =
@@ -45,17 +45,17 @@ type UnitTest() =
         let series = (1 - 2*x**2) ** 3
         Assert.AreEqual(
             [1; 0; -6; 0; 12; 0; -8; 0; 0; 0],
-            series |> PowerSeries.take 10)
+            series.Take(10))
 
         let series = 1 / (1-x)
         Assert.AreEqual(
             [1; 1; 1; 1; 1; 1; 1; 1; 1; 1],
-            series |> PowerSeries.take 10)
+            series.Take(10))
 
         let series = 1 / (1-x)**2
         Assert.AreEqual(
             [1; 2; 3; 4; 5; 6; 7; 8; 9; 10],
-            series |> PowerSeries.take 10)
+            series.Take(10))
 
     [<TestMethod>]
     member __.Calculus() =
@@ -64,19 +64,19 @@ type UnitTest() =
         let series = x * x |> PowerSeries.deriv
         Assert.AreEqual(
             [0; 2; 0],
-            series |> PowerSeries.take 3)
+            series.Take(3))
 
         let series = series |> PowerSeries.integral
         Assert.AreEqual(
             [0; 0; 1],
-            series |> PowerSeries.take 3)
+            series.Take(3))
 
     [<TestMethod>]
     member __.Exp() =
         let expected = [1N; 1N; 1N/2N; 1N/6N; 1N/24N; 1N/120N; 1N/720N]
         Assert.AreEqual(
             expected,
-            PowerSeries.exp<BigRational> |> PowerSeries.take expected.Length)
+            PowerSeries.exp<BigRational>.Take(expected.Length))
         Assert.AreEqual(
             Math.E,
             PowerSeries.exp<float>
@@ -90,11 +90,10 @@ type UnitTest() =
     [<TestMethod>]
     member __.Trig() =
 
-        let test seriesA seriesB =
-            let take = PowerSeries.take 30
+        let test (seriesA : PowerSeries<_>) (seriesB : PowerSeries<_>) =
             Assert.AreEqual(
-                take seriesA,
-                take seriesB)
+                seriesA.Take(30),
+                seriesB.Take(30))
 
         test
             PowerSeries.sin
@@ -114,8 +113,7 @@ type UnitTest() =
         let expected = [1N; 1N/2N; -1N/8N; 1N/16N]
         Assert.AreEqual(
             expected,
-            sqrt1PlusX
-                |> PowerSeries.take expected.Length)
+            sqrt1PlusX.Take(expected.Length))
         Assert.AreEqual(
             sqrt 2.0,
             sqrt1PlusX
